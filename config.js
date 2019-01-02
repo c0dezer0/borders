4e312cf7dbc788ff1e829423264e31afe1cdf876
Manish Kumar <mail2manish15@gmail.com>
2019-01-02 23:52:42 +0530

    changes

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ module.exports = {
     "port": process.env.PORT || '9000',
     "env": process.env.NODE_ENV || "development",
     "host": "localhost",
-    "dataCollector":{
-    	"keywords": ['pakistan','terror', 'terrorist','pak', 'baloch','surgical','uri','baramulla','attack','modi', 'demonetisation', 'kill', 'cash']
+    "dataCollector": {
+        "keywords": ['pakistan', 'terror', 'terrorist', 'pak', 'baloch', 'surgical', 'uri', 'baramulla', 'attack', 'modi', 'demonetisation', 'kill', 'cash']
     },
     "cron": "*/5 * * * *", // every 10 minutes
     "pushNotificationKey": "AIzaSyBkzhOxrf7frA3gn66xfdGgXRiS4ou3kJU"
